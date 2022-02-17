e1400c6eb79a012324c265eadc9e1e07a713fb44
najmudheen <iamnajmudheen7311@gmail.com>
2022-02-17 17:17:21 +0530

    init

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,37 @@
+import androidx.compose.material.MaterialTheme
+import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+@Composable
+@Preview
+fun App() {
+    MaterialTheme {
+        Box(contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize()
+            ){
+
+            val stopwatch = remember { StopWatch() }
+            StopWatchDisplay(
+                formattedTime = stopwatch.formattedTime,
+                onStartClick = stopwatch::start,
+                onStopClick = stopwatch::stop,
+                onRestartClick = stopwatch::reset
+            )
+        }
+
+    }
+}
+
+fun main() = application {
+    Window(onCloseRequest = ::exitApplication) {
+        App()
+    }
+}
